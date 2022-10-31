eb857af85bf64bf5a3962b6c6d7982a6ddd20679
Arman Abbasian <105098083+Arman-Abbasian@users.noreply.github.com>
2022-10-31 15:04:57 +0330

    initiate the project

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+import { Provider } from "react-redux";
+import {store} from './components/features/store';
+import TodoFormReduxToolkit from './components/TodoFormReduxToolKit/TodoFormReduxToolKit';
+import TodoTotalReduxToolKit from "./components/TodoTotalReduxToolKit/TodoTotalReduxToolKit";
+import TodoListReduxToolKit from "./components/TodoListReduxToolKit/TodoListReduxToolKit";
+
+import TodoTotalReducer from './components/TodoTotalReducer/TodoTotalReducer';
+import TodoListReducer from "./components/TodoListReducer/TodoListReducer";
+import TodosProvider from "./components/Providers/todosProvider";
+import TodoFormReducer from "./components/TodoFormReducer/TodoFormReducer";
+
+
+
+
+function App() {
+  return(
+    // <Provider store={store}>
+    //   <div className="flex justify-between items-center">
+    //   <TodoFormRedux />
+    //   <TodoTotalRedux />
+    //   </div>
+    //   <TodoListRedux />
+    // </Provider>
+
+    <Provider store={store}>
+      <div className="flex justify-between items-center">
+        <TodoFormReduxToolkit />
+        <TodoTotalReduxToolKit />
+      </div>
+      <TodoListReduxToolKit />
+    </Provider>
+
+    // <TodosProvider>
+    //   <div className="flex justify-between items-center">
+    //    <TodoFormReducer />
+    //    <TodoTotalReducer />
+    //   </div>
+    //   <TodoListReducer />
+    // </TodosProvider>
+  )
+}
+
+export default App;
